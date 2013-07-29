4affb6505d95f475a244601862291c5ac31feab3
Ferran Pelayo Monfort <ferran91@gmail.com>
2013-07-29 11:00:38 +0200

    Added oh-my-zsh/custom and modified install

---
+++ oh-my-zsh/custom/bindkeys.zsh
@@ -0,0 +1,7 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+bindkey '^P' up-line-or-search
+bindkey '^N' down-line-or-search
