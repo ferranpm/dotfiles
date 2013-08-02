baf83dca00dc421485589024ec8577cb31211af1
Ferran Pelayo Monfort <ferran91@gmail.com>
2013-08-02 04:16:51 +0200

    No more oh-my-zsh

--- oh-my-zsh/custom/bindkeys.zsh
+++
@@ -1,7 +0,0 @@
-# Add yourself some shortcuts to projects you often work on
-# Example:
-#
-# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
-#
-bindkey '^P' up-line-or-search
-bindkey '^N' down-line-or-search
