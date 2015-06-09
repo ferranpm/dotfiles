887d286709c92ff40734956bd8575f222ee9098a
Ferran Pelayo Monfort <ferran.pel.mon@gmail.com>
2015-06-09 16:18:24 +0200

    simplify keyboard.sh

--- bin/keyboard.sh
+++ bin/keyboard.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-killall xcape
-killall xmodmap
+pkill xcape
+pkill xmodmap
 
-xmodmap -e "remove Lock    = Caps_Lock"
-xmodmap -e "remove Control = Control_L"
+xmodmap -e "remove Lock      = Caps_Lock"
+xmodmap -e "remove Control   = Control_L"
+xmodmap -e "keysym Control_L = Caps_Lock"
+xmodmap -e "keysym Caps_Lock = Control_L"
+xmodmap -e "add Lock         = Caps_Lock"
+xmodmap -e "add Control      = Control_L"
 
-xmodmap -e "keycode 9  = Caps_Lock"
-xmodmap -e "keycode 66 = Control_L"
-xmodmap -e "keycode 37 = Escape"
-
-xmodmap -e "add Lock    = Caps_Lock"
-xmodmap -e "add Control = Control_L"
-
-xcape -e "Control_L=Escape"
+xcape
