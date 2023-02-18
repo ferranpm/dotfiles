014046c79f5073c29b9670019e7ca35f1aac1649
Ferran Pelayo Monfort <ferran.pelayo@sequra.es>
2023-02-18 09:29:40 +0100

    vim: use lua for lsp autocmd configuration

--- vim/plugin/lsp.lua
+++ vim/plugin/lsp.lua
@@ -2,17 +2,15 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', { noremap = true })
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { noremap = true })
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gu', '<cmd>lua vim.lsp.buf.references()<CR>', { noremap = true })
-
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
-vim.cmd([[
-  command! LspCodeAction lua vim.lsp.buf.code_action()
-  augroup NvimLsp
-  autocmd!
-  autocmd DiagnosticChanged * lua vim.diagnostic.setloclist({ open = false })
-  augroup END
-]])
+vim.api.nvim_create_autocmd(
+  { "DiagnosticChanged" },
+  {
+    pattern = { "*" },
+    callback = function() vim.diagnostic.setloclist({ open = false }) end
+  }
+)
 
 vim.diagnostic.config({
   virtual_text = false,
